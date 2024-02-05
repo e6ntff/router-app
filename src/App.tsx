fe295363a15b6ce1703bdd36fcd014cb0eac4159
e6ntff <pashatar2@gmail.com>
2024-02-05 18:32:55 +0300

    added hashrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import AppRouter from './components/AppRouter';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Header from './components/Header';
 
 const App: React.FC = () => {
 	return (
-		<BrowserRouter>
+		<HashRouter>
 			<Header />
 			<AppRouter></AppRouter>
-		</BrowserRouter>
+		</HashRouter>
 	);
 };
 
